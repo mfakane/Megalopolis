97520a8c0cb9abdf94043ad12f10719590c7d4fc
mfakane <star@glasscore.net>
2012-09-02 08:41:26 +0900

    管理者ログイン時既存の作品編集画面でエラーが出ることがあるのを修正

--- req/Handler/Read.php
+++ req/Handler/Read.php
@@ -169,7 +169,7 @@ class ReadHandler extends Handler
 		self::setValues($this->entry, $this->thread);
 		Visualizer::$data = self::checkValues($this->entry, $this->thread, true);
 		
-		if ($type != Util::HASH_TYPE_LATEST)
+		if (isset($type) && $type != Util::HASH_TYPE_LATEST)
 			Visualizer::$data[] = "サーバに保存されている編集キーの形式が古いため、編集キーを再度入力するか変更することを推奨します。";
 		
 		if (!is_null($_page) ||
